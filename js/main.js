51ed394db30e70ba4858cd906511394bfdbfe15f
Fokrul <fokrulbhuiyan1@gmail.com>
2026-06-15 23:22:41 +0600

    update cv and some changes

--- js/main.js
+++ js/main.js
@@ -6,4 +6,4 @@ $('.menu-toggle').on("click",function(){$('#site_header').addClass('animate');$(
 '<div class="mfp-close"></div>'+
 '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
 '<div class="mfp-title mfp-bottom-iframe-title"></div>'+
-'</div>',patterns:{youtube:{index:'youtube.com/',id:null,src:'%id%?autoplay=1'},vimeo:{index:'vimeo.com/',id:'/',src:'//player.vimeo.com/video/%id%?autoplay=1'},gmaps:{index:'//maps.google.',src:'%id%&output=embed'}},srcAction:'iframe_src',},callbacks:{markupParse:function(template,values,item){values.title=item.el.attr('title');}},});});})(jQuery);
+'</div>',patterns:{youtube:{index:'youtube.com/',id:null,src:'%id%?autoplay=1'},vimeo:{index:'vimeo.com/',id:'/',src:'//player.vimeo.com/video/%id%?autoplay=1'},gmaps:{index:'//maps.google.',src:'%id%&output=embed'}},srcAction:'iframe_src',},callbacks:{markupParse:function(template,values,item){values.title=item.el.attr('title');}},});});})(jQuery); 
